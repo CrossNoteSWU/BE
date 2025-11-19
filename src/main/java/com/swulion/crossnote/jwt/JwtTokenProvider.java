0c67e9b143ad7f32fea00e45b0c89de364c2fa9c
Yang-Boyun <boyunyang@naver.com>
2025-11-19 21:14:26 +0900

    fix(JwtTokenProvider) : base64->UTF_8 변경

--- src/main/java/com/swulion/crossnote/jwt/JwtTokenProvider.java
+++ src/main/java/com/swulion/crossnote/jwt/JwtTokenProvider.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Slf4j
@@ -25,7 +26,7 @@ public class JwtTokenProvider {
     public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey,
                             @Value("${jwt.access-token-expiration-time}") long accessTokenExpirationTime,
                             @Value("${jwt.refresh-token-expiration-time}") long refreshTokenExpirationTime) {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.accessTokenExpirationTime = accessTokenExpirationTime;
         this.refreshTokenExpirationTime = refreshTokenExpirationTime;
