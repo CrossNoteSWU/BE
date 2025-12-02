17fe895f34c7f020f5ac43e4893a53d59e2cc395
Yang-Boyun <boyunyang@naver.com>
2025-12-02 19:01:52 +0900

    feat : mypage에 분야 추가

--- src/main/java/com/swulion/crossnote/dto/MyPage/MyProfileResponseDto.java
+++ src/main/java/com/swulion/crossnote/dto/MyPage/MyProfileResponseDto.java
@@ -3,6 +3,7 @@ package com.swulion.crossnote.dto.MyPage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -14,6 +15,10 @@ public class MyProfileResponseDto {
     private String profileImageUrl;
     private long followersCount;
     private long followingsCount;
+    
+    // 관심 분야, 전문 분야
+    private List<String> interestNames;
+    private List<String> expertiseNames;
     
     // 지식 리포트 데이터
     private Map<String, Integer> knowledgeScores; // 6개 상위 분야별 점수 (0-50)
